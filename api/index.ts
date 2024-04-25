418b01e3e782b41ee2d3ca04bb9ace2df8fc850b
wanghaisheng <edwin.uestc@gmail.com>
2024-04-26 07:03:08 +0800

    disable options

--- api/index.ts
+++ api/index.ts
@@ -29,84 +29,87 @@ export default async function handler(
   response: NextApiResponse
 ) {
   // replace /api/
-  let inputKeywords = request.url?.replace("/api/kd", "")
+  if (request.url?.includes('/api/kd')) {
 
-  if (!inputKeywords) {
-    response.status(400).send("Missing inputKeywords parameter")
-    return
-  }
+    let inputKeywords = request.url?.replace("/api/kd", "")
 
-  // Example usage
-  // const inputKeywords = "sectional sofa"
-  let formattedKeywords
-  if (inputKeywords.includes(" ")) {
-    formattedKeywords = formatKeywords(inputKeywords)
-    console.log("Formatted keywords:", formattedKeywords)
-  } else {
-    formattedKeywords = inputKeywords
-    console.log("No spaces found in inputKeywords.")
-  }
-  let url = 'https://ahrefs.com/keyword-difficulty/'
-  try {
-    const browser = await playwright.launch({
-      args: sparticuzChromium.args,
-
-      executablePath: await sparticuzChromium.executablePath("https://github.com/Sparticuz/chromium/releases/download/v123.0.0/chromium-v123.0.0-pack.tar"),
-      headless: sparticuzChromium.headless,
-    })
-    console.log("Chromium:", await browser.version())
-
-    const context = await browser.newContext()
-    console.log("new context")
-
-    const page = await context.newPage()
-    console.log("new page")
-
-    try {
-      console.log("go to url", url)
-      await page.goto(url as string)
-
-      // await page.goto(url as string, { timeout: 60000 }) // 60 seconds timeout
-
-      console.log(await page.title())
-      // Rest of your code
-
-      await page.getByPlaceholder('Enter keyword').click()
-
-      await page
-        .getByPlaceholder('Enter keyword')
-        .fill(inputKeywords)
-      console.log("fill keyword", inputKeywords)
-
-      // Start waiting for new page before clicking. Note no await.
-      const pagePromise = context.waitForEvent('page')
-      await page.getByRole('button', { name: 'Check keyword' }).click()
-      console.log("click submit")
-
-      const newPage = await pagePromise
-      await newPage.waitForLoadState()
-      console.log(await newPage.title())
-
-      console.log(newPage.url())
-
-      const pdfBytes = await newPage.content()
-      await browser.close()
-
-      let fileName = formattedKeywords + ".html"
-
-      response.setHeader("Content-Type", "application/html")
-      response.setHeader(
-        "Content-Disposition",
-        'inline; filename="' + fileName + '"'
-      )
-
-      response.status(200).send(pdfBytes)
-
-    } catch (error) {
-      console.error('Navigation error:', error)
-      // Handle the error appropriately
+    if (!inputKeywords) {
+      response.status(400).send("Missing inputKeywords parameter")
+      return
     }
-  } catch (error: any) {
-    response.status(500).json({ error: error.message })
+
+    // Example usage
+    // const inputKeywords = "sectional sofa"
+    let formattedKeywords
+    if (inputKeywords.includes(" ")) {
+      formattedKeywords = formatKeywords(inputKeywords)
+      console.log("Formatted keywords:", formattedKeywords)
+    } else {
+      formattedKeywords = inputKeywords
+      console.log("No spaces found in inputKeywords.")
+    }
+    let url = 'https://ahrefs.com/keyword-difficulty/'
+    try {
+      const browser = await playwright.launch({
+        args: sparticuzChromium.args,
+
+        executablePath: await sparticuzChromium.executablePath("https://github.com/Sparticuz/chromium/releases/download/v123.0.0/chromium-v123.0.0-pack.tar"),
+        headless: sparticuzChromium.headless,
+      })
+      console.log("Chromium:", await browser.version())
+
+      const context = await browser.newContext()
+      console.log("new context")
+
+      const page = await context.newPage()
+      console.log("new page")
+
+      try {
+        console.log("go to url", url)
+        await page.goto(url as string)
+
+        // await page.goto(url as string, { timeout: 60000 }) // 60 seconds timeout
+
+        console.log(await page.title())
+        // Rest of your code
+
+        await page.getByPlaceholder('Enter keyword').click()
+
+        await page
+          .getByPlaceholder('Enter keyword')
+          .fill(inputKeywords)
+        console.log("fill keyword", inputKeywords)
+
+        // Start waiting for new page before clicking. Note no await.
+        const pagePromise = context.waitForEvent('page')
+        await page.getByRole('button', { name: 'Check keyword' }).click()
+        console.log("click submit")
+
+        const newPage = await pagePromise
+        await newPage.waitForLoadState()
+        console.log(await newPage.title())
+
+        console.log(newPage.url())
+
+        const pdfBytes = await newPage.content()
+        await browser.close()
+
+        let fileName = formattedKeywords + ".html"
+
+        response.setHeader("Content-Type", "application/html")
+        response.setHeader(
+          "Content-Disposition",
+          'inline; filename="' + fileName + '"'
+        )
+
+        response.status(200).send(pdfBytes)
+
+      } catch (error) {
+        console.error('Navigation error:', error)
+        // Handle the error appropriately
+      }
+    } catch (error: any) {
+      response.status(500).json({ error: error.message })
+    }
   }
 }
